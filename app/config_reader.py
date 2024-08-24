47e18ebbfe261c4bad399fa0dae03f0e8ed345a2
Sergey Kovalevich <inndie@gmail.com>
2024-08-24 11:29:56 +0300

    rever

--- app/config_reader.py
+++ app/config_reader.py
@@ -28,7 +28,7 @@ def load_config() -> Config:
     config=Config(
         telegram=TelegramConfig(
             token=parser.get('telegram', 'token'),
-            chat_id=parser.get('telegram', 'chat_id')
+            chat_id=int(parser.get('telegram', 'chat_id'))
         ),
         kadinsky=KadinskyConfig(
             key=parser.get('kadinsky', 'key'),
